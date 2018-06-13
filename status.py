0c3f1388078b734fee3598f8f94bcd71ffcedf04
Valmik Jangla <mail@valmik.in>
2018-06-13 13:27:25 +0530

    Removed useless class

--- status.py
+++ status.py
@@ -17,16 +17,3 @@ for app in os.listdir('./apps'):
 					cwd=os.path.join('apps', app), stdout=subprocess.PIPE)
 			(out, err) = p.communicate()
 			print "\033[1m %-45s \033[0m \033[1m \033[94m %15s \033[0m" % (app, out.rstrip().replace("##", ""))
-
-
-class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
